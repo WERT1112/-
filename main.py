29f3c251ac499ad47a6d676a52699ab268931dc6
WERT1112 <113766282+WERT1112@users.noreply.github.com>
2022-09-17 13:16:09 +0300

    ПЕРВЫЙ КОД УРОКА
    ПОЙСКОВИК ЧИСЕЛ 1000-7

---
+++ main.py
@@ -0,0 +1,21 @@
+a= int(input("введите А:"))
+b= int(input("введите B:"))
+c= int(input("введите C:"))
+
+if (a>b and a>c):
+    print("А больше всех. А= ", a)
+elif (b>a and b>c):
+    print("В больше всех. В= ", a)
+elif (c>a and c>b):
+    print("с больше всех. С= ", a)
+else:
+    print(" ОНИ РАВНЫ")
+
+
+
+
+
+
+
+
+
